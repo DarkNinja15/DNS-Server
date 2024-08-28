56087642c712d34734deffd265f0406a86417321
Animesh Shukla <animeshshukla1518@gmail.com>
2024-08-28 13:02:06 +0530

    chore: connected a udp server

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import * as dgram from "dgram";
+
+const udpSocket: dgram.Socket = dgram.createSocket("udp4");
+udpSocket.bind(2053, "127.0.0.1",()=>{
+    console.log("Server connected on port 2053");
+});
+
+udpSocket.on("message", (data: Buffer, remoteAddr: dgram.RemoteInfo) => {
+    try {
+        console.log(`Received data from ${remoteAddr.address}:${remoteAddr.port}`);
+        const response = Buffer.from("");
+        udpSocket.send(response, remoteAddr.port, remoteAddr.address);
+    } catch (e) {
+        console.log(`Error sending data: ${e}`);
+    }
+});
